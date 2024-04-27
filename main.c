0d33001d9771622c112bb59fe3c182f5e2d085b1
Ruan Correia <correia.ruan@gmail.com>
2024-04-27 18:59:56 -0300

    Criacao de menu

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+ int menu;
  
-void main()
-{
-  printf("Hello Geek!");
+int main(){
+while(menu != 0){
+  printf("Escolha uma opção no menu abaixo: \n");
+  printf("1- Mostrar calendário\n 2- Inserir agendamento\n 3- Mostrar calendário com agendamento\n 0- Sair");
+}
+
+  return 0;
 }
