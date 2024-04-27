a87d5313e6536fb9a27e32cb1720a7eb63aee275
Ruan Correia <correia.ruan@gmail.com>
2024-04-27 19:01:08 -0300

    .

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main(){
 while(menu != 0){
   printf("Escolha uma opção no menu abaixo: \n");
   printf("1- Mostrar calendário\n 2- Inserir agendamento\n 3- Mostrar calendário com agendamento\n 0- Sair");
+  
 }
 
   return 0;
