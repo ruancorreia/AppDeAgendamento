fa06fc760fef5c515d6927c6cdbef8300d365d85
Ruan Correia <correia.ruan@gmail.com>
2024-04-27 20:19:56 -0300

    Menu criado

--- main.c
+++ main.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
- int menu;
- 
-int main(){
-while(menu != 0){
-  printf("Escolha uma opção no menu abaixo: \n");
-  printf("1- Mostrar calendário\n 2- Inserir agendamento\n 3- Mostrar calendário com agendamento\n 0- Sair");
-}
+
+int main()
+{
+  int menu = 1; 
+
+  printf("== FISIOGENDA AGENDAMENTO INTELIGENTE ==\n");
+  while (menu != 0) 
+  {
+    printf("Selecione uma opção para prosseguir:\n");
+    printf("1 - Mostrar Calendário\n2 - Inserir Agendamento\n3 - Mostrar Calendário com agendamentos\n0 - Sair\n");
+
+
+    scanf("%d", &menu);
+
+    switch (menu) 
+    {
+    case 1:
+      printf("Mostrar Calendário\n");
+      break;
+    case 2:
+      printf("Inserir Agendamento\n");
+      break;
+    case 3:
+      printf("Mostrar Calendário com agendamentos\n");
+      break;
+    case 0:
+      printf("Saindo do programa\n");
+      break;
+    default:
+      printf("Opção inválida\n");
+      break;
+    }
+  }
 
   return 0;
 }
