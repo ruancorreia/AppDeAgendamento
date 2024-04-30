2b3f0709ea07dbcce886f5fe5d38c5f60c276037
Ruan Correia <correia.ruan@gmail.com>
2024-04-29 21:24:37 -0300

    WORK CHECKPOINT
    Adicionado menu no local correto e criando um backup para retornar caso algo de errado

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <locale.h>
+
 
 int dias_no_mes[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
@@ -18,46 +20,11 @@ char meses[][20] = {
     "\n\n\nDezembro"
 };
 
-int main(void)
-{
-  /*
-  Espaço para colocar as funções criadas
-  */
-
-  int escolha;
-  do
-  {
-    printf("\n\n\t== FIOSIOGENDA ==\n");
-    printf("SELECIONE ABAIXO UMA DAS OPÇÕES:\n\n");
-    printf("1 - Mostrar Calendário\n");
-    printf("2 - Agendar Tratamento\n");
-    printf("3 - Sair\n");
-    printf("Digite sua escolha: ");
-    scanf("%d", &escolha);
-
-    switch (escolha)
-    {
-    case 1:
-      printf("Função exibir_calendario()");
-      break;
-    case 2:
-      printf("Função agendar_compromisso()");
-      break;
-    case 3:
-      printf("Programa finalizado com sucesso...\n");
-      break;
-    default:
-      printf("Escolha inválida! Por favor, tente novamente.\n");
-    }
-  } while (escolha != 3);
-
-  return 0;
-}
-
-/*Esta funçao tem o objetivo de pedir ao usuario que insira o primeiro dia do ano indicando o dia da semana em que ele vai cair
-Exemplo: ao selecionar 0, o primeiro dia da semana sera domingo, 1º de janeiro.
-*/
-int inserir_dia_inicio(void)
+    /*
+    Esta funçao tem o objetivo de pedir ao usuario que insira o primeiro dia do ano indicando o dia da semana em que ele vai cair
+    Exemplo: ao selecionar 0, o primeiro dia da semana sera domingo, 1º de janeiro.
+    */
+    int inserir_dia_inicio(void)
 {
   int dia;
   printf("Por favor, insira o dia da semana em que cai o dia 1º de Janeiro (0 para Domingo, 1 para Segunda-feira, ..., 6 para Sábado): ");
@@ -66,7 +33,8 @@ int inserir_dia_inicio(void)
 }
 
 
-/*Esta funçao irá perguntar ao usuário se o ano é bissexto ou nao
+/*
+Esta funçao irá perguntar ao usuário se o ano é bissexto ou nao
 retornando 1 para caso positivo e 0 para caso negativo.
 */
 int inserir_ano_bissexto(void)
@@ -87,7 +55,8 @@ int determinar_codigo_dia(int dia_inicio)
 }
 
 
-/*Esta funçao irá verificar se o ano foi definido como bissexto ou nao
+/*
+Esta funçao irá verificar se o ano foi definido como bissexto ou nao
 e irá ajustar os dias de fevereiro de acordo
 */
 int determinar_ano_bissexto(int bissexto, int dias_no_mes[]) {
@@ -100,8 +69,10 @@ int determinar_ano_bissexto(int bissexto, int dias_no_mes[]) {
     }
 }
 
-
-// Esta funçao irá exibir os calendários - Regular e de Compromissos Agendados.
+/*
+Esta funçao irá percorrer os arrays e exibir os calendários 
+Regular e de Compromissos Agendados.
+*/
 void exibir_calendario(int dia_inicio, int bissexto, int compromissos[], int dias_no_mes[]) {
     int mes, dia;
     int dias_no_mes_temp[13];
@@ -138,7 +109,7 @@ void exibir_calendario(int dia_inicio, int bissexto, int compromissos[], int dia
 
     printf("Calendário de Compromissos:\n");
 
-    // Exibir calendário de compromissos
+    // Exibir calendário de compromissos agendados 
     dia_inicio_mes = dia_inicio; // Resetar dia de início
     for (mes = 1; mes <= 12; mes++) {
         printf("%s", meses[mes]);
@@ -165,4 +136,92 @@ void exibir_calendario(int dia_inicio, int bissexto, int compromissos[], int dia
         // Calcular o dia de início para o próximo mês
         dia_inicio_mes = (dia_inicio_mes + dias_no_mes[mes]) % 7;
     }
+}
+// Função para agendar um compromisso
+/*
+Função criada para fazer o agendamento dos procedimentos,
+ela vai pegar o mes e o dia (validando se esta corretamente preenchido)
+e validando se nao há mais do que 5 agendamentos naquele dia, caso haja, irá apresentar uma mensagem
+informando que ja esta no limite.
+*/
+void agendar_compromisso(int compromissos[], int ano, int dias_no_mes[])
+{
+  int mes, dia, duracao;
+
+  printf("Digite o mês (1-12) do compromisso: ");
+  scanf("%d", &mes);
+  printf("Digite o dia (1-%d) do compromisso: ", dias_no_mes[mes]);
+  scanf("%d", &dia);
+
+  // Validar a entrada do dia
+  if (dia < 1 || dia > dias_no_mes[mes])
+  {
+    printf("Dia inserido inválido!\n");
+    return;
+  }
+
+  // Calcular o índice no array de compromissos
+  int indice = (mes - 1) * 31 + (dia - 1);
+
+  // Verificar se já existem compromissos marcados para este dia
+  if (compromissos[indice] >= 5)
+  {
+    printf("Desculpe, máximo de compromissos alcançado para este dia!\n");
+    return;
+  }
+
+  printf("Digite a duração do tratamento (em dias): ");
+  scanf("%d", &duracao);
+
+  // Atualizar o array de compromissos para a duração especificada
+  for (int i = 0; i < duracao; i++)
+  {
+    if (indice + i >= 12 * 31)
+    {
+      printf("Duração do tratamento excede o ano!\n");
+      return;
+    }
+    compromissos[indice + i] += 1; // Incrementar contagem de compromissos para cada dia do tratamento
+  }
+
+  printf("Compromisso agendado com sucesso!\n");
+}
+
+int main(void)
+{
+  int dia_inicio, ano_bissexto;
+  dia_inicio = inserir_dia_inicio();
+  ano_bissexto = inserir_ano_bissexto();
+
+  // Array para armazenar compromissos, um para cada dia do ano
+  int compromissos[12 * 31] = {0}; // Inicializar todas as contagens para 0
+
+  int escolha;
+  do
+  {
+    printf("\n\t== FIOSIOGENDA ==\n");
+    printf("MENU:\n");
+    printf("1. Mostrar Calendário\n");
+    printf("2. Agendar Tratamento\n");
+    printf("3. Sair\n");
+    printf("Digite sua escolha: ");
+    scanf("%d", &escolha);
+
+    switch (escolha)
+    {
+    case 1:
+      exibir_calendario(dia_inicio, ano_bissexto, compromissos, dias_no_mes);
+      break;
+    case 2:
+      agendar_compromisso(compromissos, ano_bissexto, dias_no_mes);
+      break;
+    case 3:
+      printf("Programa finalizado com sucesso...\n");
+      break;
+    default:
+      printf("Escolha inválida! Por favor, tente novamente.\n");
+    }
+  } while (escolha != 3);
+
+  return 0;
 }
